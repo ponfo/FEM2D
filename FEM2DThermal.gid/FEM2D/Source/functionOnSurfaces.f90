5e39861c7940374cc5cded74a39238917ce49ba2
ponfo <airaudofacundo@gmail.com>
2019-11-20 22:04:27 -0300

    6 benchmarks working

--- FEM2DThermal.gid/FEM2D/Source/functionOnSurfaces.f90
+++ FEM2DThermal.gid/FEM2D/Source/functionOnSurfaces.f90
@@ -7,7 +7,8 @@ contains
   integer(ikind), intent(in) :: i
   real(rkind), intent(in) :: x
   real(rkind), intent(in) :: y
-  real(rkind) :: vecFunSource(0)
+  real(rkind) :: vecFunSource(1)
+  vecFunSource(1)=   30
   funcOnSurfaces = vecfunSource(i)
   end function funcOnSurfaces
 end module FunctionOnSurfacesMOD
