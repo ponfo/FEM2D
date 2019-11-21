c492d088e01be01f6f18f34350368c300ee2ee1c
Marco1410 <mzuniga433@alumnos.iua.edu.ar>
2019-11-21 11:53:03 -0300

    ordenado

--- FEM2DThermal.gid/precomp.f90
+++ FEM2DThermal.gid/precomp.f90
@@ -38,7 +38,7 @@ program precomp
      read(functionData,*)
   end do
 
-  open(function, file = trim(path)//'/FEM2D/Source/functionOnPoints.f90')
+  open(function, file = trim(path)//'/FEM2D/Source/Source/functionOnPoints.f90')
   write(function,'(A)')       'module FunctionOnPointsMOD'
   write(function,'(A)')       '  use tools'
   write(function,'(A)')       '  implicit none'
@@ -65,7 +65,7 @@ program precomp
      read(functionData,*)
   end do
 
-  open(function, file = trim(path)//'/FEM2D/Source/functionOnLines.f90')
+  open(function, file = trim(path)//'/FEM2D/Source/Source/functionOnLines.f90')
   write(function,'(A)')       'module FunctionOnLinesMOD'
   write(function,'(A)')       '  use tools'
   write(function,'(A)')       '  implicit none'
@@ -92,7 +92,7 @@ program precomp
      read(functionData,*)
   end do
 
-  open(function, file = trim(path)//'/FEM2D/Source/functionOnSurfaces.f90')
+  open(function, file = trim(path)//'/FEM2D/Source/Source/functionOnSurfaces.f90')
   write(function,'(A)')       'module FunctionOnSurfacesMOD'
   write(function,'(A)')       '  use tools'
   write(function,'(A)')       '  implicit none'
