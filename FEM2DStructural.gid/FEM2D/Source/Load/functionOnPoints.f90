5c92debc501e5664d9d6b2108a53e8fb6eb184e2
¨marco1410¨ <mzuniga433@alumnos.iua.edu.ar>
2020-02-26 21:27:50 -0300

    benchstructural

--- FEM2DStructural.gid/FEM2D/Source/Load/functionOnPoints.f90
+++ FEM2DStructural.gid/FEM2D/Source/Load/functionOnPoints.f90
@@ -7,9 +7,19 @@ module FunctionOnPointsMOD
   integer(ikind), intent(in) :: i
   real(rkind), intent(in) :: x
   real(rkind), intent(in) :: y
-  real(rkind) :: vecFunX(0)
-  real(rkind) :: vecFunY(0)
+  real(rkind) :: vecFunX(5)
+  real(rkind) :: vecFunY(5)
   real(rkind), dimension(2) :: funcOnPoints
+  vecFunX(1)=       0.0
+  vecFunY(1)=     -2000
+  vecFunX(2)=       0.0
+  vecFunY(2)=     -2000
+  vecFunX(3)=       0.0
+  vecFunY(3)=     -2000
+  vecFunX(4)=       0.0
+  vecFunY(4)=     -2000
+  vecFunX(5)=       0.0
+  vecFunY(5)=     -2000
   funcOnPoints(1) = vecFunX(i)
   funcOnPoints(2) = vecFunY(i)
   end function funcOnPoints
